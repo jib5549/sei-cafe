119448292586738b85c4ab43144efdd862b4ab51
jib5549 <jib5549@gmail.com>
2023-08-19 17:00:20 -0400

    sei-cafe working

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+require('./config/database')
+const PORT = process.env.PORT || 8000;
+const app = require('./app-server')
+
+
+app.listen(PORT, () => {
+	console.log('We in the building on ' + PORT)
+})
